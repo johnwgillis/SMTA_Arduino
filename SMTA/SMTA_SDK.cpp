5f05045f072dcb69051708fe1cf341880f42cea3
Grant Hilgert <Grant@mwc-108246.dhcp.missouri.edu>
2017-10-25 11:27:27 -0500

    Added pin definitions

--- SMTA/SMTA_SDK.cpp
+++ SMTA/SMTA_SDK.cpp
@@ -1,5 +1,19 @@
 #include "Arduino.h"
 
+#define LED1 23
+#define LED2 22
+#define resetSwitch 21
+#define powerSwitch
+#define kill 17
+#define STAT1 18
+#define STAT2 19
+#define VeeSense 27
+#define VccSense 25
+#define ISense 26
+#define gridEyeInt 32
+#define gridEyeSCLv 35
+#define gridEyeSDA 34
+
 int greenLed(); //Turn green LED on or off
 int amberLed(); //Turn amber LED on or off
 
@@ -10,8 +24,8 @@ int getVee();//Return (1/2)*Vee voltage
 int getVcc(); //Return (1/2)*Vcc voltage
 int powerdown(); //Tell supervisor to cut power
 
-int resetSwitch(); //Function to run at resetSwitch interupt
-int powerSwitch(); //Function to run at powerSwitch interupt
+int networkReset(); //Function to run at resetSwitch interupt
+int userInterupt(); //Function to run at powerSwitch interupt
 
 
 
