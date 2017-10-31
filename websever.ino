3204f9ab7d9ce2eb76a22823831974dc9d2764c9
hhw98 <31592889+hhw98@users.noreply.github.com>
2017-10-31 16:12:54 -0500

    Add files via upload

---
+++ websever.ino
@@ -0,0 +1,174 @@
+/*
+ WiFi Web Server LED Blink
+
+ A simple web server that lets you blink an LED via the web.
+ This sketch will print the IP address of your WiFi Shield (once connected)
+ to the Serial monitor. From there, you can open that address in a web browser
+ to turn on and off the LED on pin 5.
+
+ If the IP address of your shield is yourAddress:
+ http://yourAddress/H turns the LED on
+ http://yourAddress/L turns it off
+
+ This example is written for a network using WPA encryption. For
+ WEP or WPA, change the Wifi.begin() call accordingly.
+
+ Circuit:
+ * WiFi shield attached
+ * LED attached to pin 5
+
+ created for arduino 25 Nov 2012
+ by Tom Igoe
+
+ported for sparkfun esp32 
+31.01.2017 by Jan Hendrik Berlin
+ 
+ */
+
+ // use first channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0     0
+
+// use 13 bit precission for LEDC timer
+#define LEDC_TIMER_13_BIT  13
+
+// use 5000 Hz as a LEDC base frequency
+#define LEDC_BASE_FREQ     5000
+
+// fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
+#define LED_PIN            2
+
+int brightness ;    // how bright the LED is
+int fadeAmount = 50;    // how many points to fade the LED by
+
+// Arduino like analogWrite
+// value has to be between 0 and valueMax
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+  // calculate duty, 8191 from 2 ^ 13 - 1
+  uint32_t duty = (8191 / valueMax) * min(value, valueMax);
+
+  // write duty to LEDC
+  ledcWrite(channel, duty);
+}
+
+#include <WiFi.h>
+
+const char* ssid     = "TigerWiFi-Guest";
+const char* password = "Discovery&Excellence";
+
+WiFiServer server(80);
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);      // set the LED pin mode
+    digitalWrite(2, 0);
+
+    delay(10);
+    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+    ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+
+}
+
+int value = 0;
+
+void loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+            client.print("Click <a href=\"/B\">here</a> to lighten the light of LED on pin 2.<br>");
+            client.print("Click <a href=\"/D\">here</a> to darken the light of LED on pin 2.<br>");
+       
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          brightness=500; // GET /H turns the LED on
+          ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
+        }
+        if (currentLine.endsWith("GET /L")) {
+          brightness=0; // GET /H turns the LED on
+          ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
+        }
+        if (currentLine.endsWith("GET /B")) {
+    
+        if (brightness >= 500) {
+              brightness=500; 
+        }
+        else{
+              brightness = brightness + fadeAmount;
+        }
+        ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
+ 
+        }
+        if (currentLine.endsWith("GET /D")) {
+            // set the brightness on LEDC channel 0
+         //ledcAnalogWrite(LEDC_CHANNEL_0, 100);
+        if(brightness <=0) {
+              brightness=0; 
+        }
+  // change the brightness for next time through the loop:
+         else{
+                brightness = brightness - fadeAmount;
+         }
+         ledcAnalogWrite(LEDC_CHANNEL_0, brightness);
+
+  
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
